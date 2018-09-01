59e5b8491653b1d8666890b22eced552d615dac0
jyh <yuhangjiang17@gmail.com>
2018-09-01 11:08:18 +0800

    test

--- 01_cas-client-server/cas-client-sequential.sh
+++ 01_cas-client-server/cas-client-sequential.sh
@@ -5,7 +5,9 @@
 ip=192.168.0.120
 port=9999
 
-rm data-cas-sequential
+if [ ls data-cas-sequential ]; then
+    rm data-cas-sequential
+fi
 
 for blocksize in 64 512 1024 2048 4096 16384 65536 131072
 do
@@ -14,3 +16,5 @@ do
         ./rdma-client read $ip $port $blocksize
     done
 done
+
+exit 0
