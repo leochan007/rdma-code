0e8020bd98f05c6f7480285b968699feba848f38
jyh <yuhangjiang17@gmail.com>
2018-09-01 11:02:04 +0800

    01 dir Update

---
+++ 01_cas-client-server/cas-client-sequential.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Program:
+#       Record data about cpu time and throughput under cas-sequential-read mode automatically
+
+ip = 192.168.0.120
+port = 9999
+
+for blocksize in 64 512 1024 2048 4096 16384 65536 131072
+do
+    for ((i = 0; i < 5; i = i + 1))
+    do
+        ./rdma-client read $ip $port $blocksize
+    done
+done
