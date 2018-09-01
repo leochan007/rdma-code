33775c33ad3d25f215568d4f5763423c74948082
jyh <yuhangjiang17@gmail.com>
2018-09-01 10:30:51 +0800

    test

--- 01_cas-client-server/rdma-client.c
+++ 01_cas-client-server/rdma-client.c
@@ -407,10 +407,11 @@ void on_completion_client(struct ibv_wc *wc)
         cycles_to_units = get_cpu_mhz(0) * 1000000;
         sum_of_test_cycles = (double)(end - start);
         double tp_avg = ((double) RDMA_BUFFER_SIZE * cycles_to_units) / (sum_of_test_cycles * 0x100000);
-        double bw_avg = ((double) RDMA_BUFFER_SIZE * cycles_to_units) / (sum_of_test_cycles * 0x100000);
+        //double bw_avg = ((double) RDMA_BUFFER_SIZE * cycles_to_units) / (sum_of_test_cycles * 0x100000);
         //printf("\nsum_of_test_cycles : %lf\n", sum_of_test_cycles);
         //printf("\ncpu time : %lf s, cpu frequency : %lf hz\n bandwidth : %lf MB/s, throughput : %lf MB/s\n", sum_of_test_cycles/cycles_to_units, cycles_to_units, bw_avg, tp_avg);
-        fprintf("cputime %lf   throughput %lf\n", sum_of_test_cycles/cycles_to_units, tp_avg);
+        fprintf(fp, "cputime %lf   throughput %lf\n", sum_of_test_cycles/cycles_to_units, tp_avg);
+        fp.close();
         rdma_disconnect(conn->id);
     }
 }
